404176dda9e5f90a3deac09f932ee971b4eb11c6
flo-ast <florence.astruc@hotmail.fr>
2022-05-06 17:40:36 +0200

    update Film

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,32 +1,63 @@
+import { useState, useEffect} from 'react';
+import axios from 'axios';
+
+/* Url de l'API */
+const urlFilms = 'https://swapi.dev/api/films';
+
+
 const Thumbnail = () => {
+    
+    const [films, setFilms] = useState([]);
+    const [onSort, setOnSort] = useState(false);
+    
+    useEffect(() => {
+
+        /**intéragis avec l'API pour récupérer des données des films */
+        const fetchFilms = () => {
+            axios.get(urlFilms).then(res => {
+                setFilms(res.data.results);
+            });
+        };
+        
+        fetchFilms();
+        console.log(onSort);
+    }, [onSort]);
+
+
+    /**change le format de la date */
+    const formattedDate = (date) => {
+        let formatted_date = date.split('-').reverse().join('/');
+        return formatted_date;
+    };
+
+    /**change l'ordre des films */
+    const onButton = () => {
+        return setOnSort(current => !current);
+    }
+    
+
     return (
-        <div>
-            <h2>title</h2>
-            <p>director</p>
-            <p>producer</p>
-            <p>opening_crawl : Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis illo, quos sunt dignissimos aperiam pariatur.</p>
-            <ul>
-                <li>characters</li>
-                <li></li>
-            </ul>
-            <ul>
-                <li>planets</li>
-                <li></li>
-            </ul>
-            <ul>
-                <li>starships</li>
-                <li></li>
-            </ul>
-            <ul>
-                <li>vehicles</li>
-                <li></li>
-            </ul>
-            <ul>
-                <li>species</li>
-                <li></li>
-            </ul>
-        </div>
-    )
+        <>
+            <div className="flex flex-row flex-nowrap justify-center align-center mb-9">
+                <p>Triez par :</p>
+                <button className={`${(onSort) ? '' : 'opacity-25 pointer-events-none '}ml-2 border-b border-transparent hover:border-b hover:border-amber-400 transition ease-in duration-200`} type="button" onClick={onButton}>Date de sortie /</button>
+                <button className={`${(onSort) ? 'opacity-25 pointer-events-none ' : ''}ml-2 border-b border-transparent hover:border-b hover:border-amber-400 transition ease-in duration-200`} type="button" onClick={onButton}>Chronologie de l'histoire</button>
+            </div>
+            <div className='flex flex-row flex-wrap justify-evenly items-stretch'>
+                {films.sort((a,b) => (onSort) ? a.episode_id - b.episode_id : a - b)
+                      .map(film => (
+                        <div key={film.episode_id} className='sm:w-2/5 lg:w-1/4 mx-5 mb-10 border border-amber-600 p-8'>
+                                <h2 className='w-4/5 mx-auto mb-5 border border-amber-400 p-4 font-bold text-center uppercase'>{film.title}</h2>
+                                <p>Episode : {film.episode_id}</p>
+                                <p>Date de sortie : {formattedDate(film.release_date)}</p>
+                                <p>Réalisateur : {film.director}</p>
+                                <p>Producteurs : {film.producer}</p>
+                                <p>Synopsis : {film.opening_crawl}</p>
+                        </div>
+                ))}
+            </div>  
+        </>
+    );
 }
 
 export default Thumbnail;
--- src/pages/Films.js
+++ src/pages/Films.js
@@ -5,7 +5,7 @@ const Films = () => {
     return (
         <div className=" text-amber-400 bg-neutral-900">
             <Navbar />
-            <h1 className="w-1/3 mx-auto mb-10 py-4 border-b-2 border-amber-600 text-center text-5xl text-amber-600 ">Films</h1>
+            <h1 className="w-1/3 mx-auto mb-24 py-4 border-b-2 border-amber-600 text-center text-5xl text-amber-600 ">Films</h1>
             <Thumbnail />
         </div>
     );
